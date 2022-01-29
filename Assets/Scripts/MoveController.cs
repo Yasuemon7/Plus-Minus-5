bca86c50d9a750c5f85e6ffb3e1b08c127dc0a08
Yasu <teruyasu39@gmail.com>
2022-01-29 22:11:17 +0900

    Create MoveController.cs

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngineUI;
+
+
+//public class NumberImageClass //追加ー加藤
+//{
+//    public List<Sprite> NumberImage;
+//}
+
+public class MoveController : MonoBehaviour
+{
+   // public string Number1; //追加ー加藤
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int index = (int)Random.Range(1.0f, 19.0f);
+
+        // Object4_1_Image.sprite = IntToSprite(Random.Range(1, 19));
+
+        // public List1 list1[19];
+
+        //List<string> Nuber1 = new List<string>();
+        //Number1.Add("Number_Zero");
+        //Number1.Add("Number_Minus-9");
+        //Number1.Add("Number_Minus-8");
+        //Number1.Add("Number_Minus-7");
+        //Number1.Add("Number_Minus-6");
+        //Number1.Add("Number_Minus-5");
+
+        //int index = (int)Random.Range(0.0f, playerName.Count + 1);
+        //string NowNumber = Nuber1[index]; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
